570f3d196ca32c4af27710fa0c5d305e58bc96c6
Daniel Frevel <daniel-frevel@web.de>
2026-02-03 14:57:45 +0100

    Starter main page

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Portfolio site dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            nodePackages.npm
+          ];
+
+          shellHook = ''
+            echo "Portfolio dev environment"
+            echo "Run: npm install && npm run dev"
+          '';
+        };
+      });
+}
